7dc75a767b57798db4beea6056d08543fdaab058
metaverde <jenniferleesilves@gmail.com>
2023-11-12 07:10:43 -0800

    More in store program

--- pythonstore.py
+++ pythonstore.py
@@ -1,4 +1,6 @@
 
+itemsDict={}
+
 #welcome user
 userName=input("Please enter your name: ")
 welcomeMessage=f"Welcome to my store {userName}"
@@ -9,7 +11,44 @@ print(welcomeMessage)
 print("*"*lenWM)
 
 my_file=open("items.txt")
-file_str=my_file.read()
-print(file_str)
+#file_str=my_file.read()
+#print(file_str)
+#file_line=my_file.readline()
+#print(file_line)
+#file_line=my_file.readline()
+#print(file_line)
 
+file_line=my_file.readline()
+items=my_file.readlines()
 my_file.close()
+
+print("**********Items Available Now!**********")
+
+for item in items:
+    item_name=item.split()[0]
+    item_price=item.split()[1]
+    print(f"{item_name}: {item_price}")
+    itemsDict.update({item_name: item_price})
+    #print(itemDict)
+print("*"*20)
+
+#prompt user to add items
+shopping=input("Would you like to shop now? (yes/no):")
+
+if shopping=="yes" or "Yes":
+##    I'm having to change a fair bit of syntax.
+##    For example shopping.lower skips to the else clause.
+##    running into the same thing with how the tutorial deals with the dictionary check.
+##    If this were my first Python rodeo I'd be tearing my hair out.
+    
+    item_added=input("What would you like?: ")
+    if item_added in itemsDict:
+        print("Thank-you for purchasing " +item_added)
+    else:
+        print("We don't have that... yet.")
+        
+    
+else:
+    print("That's cool. See you soon!")
+    
+#so far so good. Worked out some issues, (re)learning a lot. Encouraged.
