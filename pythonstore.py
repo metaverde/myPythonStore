863651be17925299e33c001c0546e7d90886e28b
metaverde <jenniferleesilves@gmail.com>
2023-11-17 19:44:59 -0800

    Happy so far

--- pythonstore.py
+++ pythonstore.py
@@ -1,5 +1,6 @@
 
 itemsDict={}
+shoppingDict={}
 
 #welcome user
 userName=input("Please enter your name: ")
@@ -28,22 +29,30 @@ for item in items:
     item_name=item.split()[0]
     item_price=item.split()[1]
     print(f"{item_name}: {item_price}")
-    itemsDict.update({item_name: item_price})
-    #print(itemDict)
+    itemsDict.update({item_name: float(item_price)})
+    #print(itemsDict)
+    #print(item_name, item_price)
+    
 print("*"*20)
 
 #prompt user to add items
 shopping=input("Would you like to shop now? (yes/no):")
+shopping=shopping.lower()
 
-if shopping=="yes" or "Yes":
+if shopping=="yes":
 ##    I'm having to change a fair bit of syntax.
-##    For example shopping.lower skips to the else clause.
+##    For example shopping.lower skips to the else clause./fixed it
 ##    running into the same thing with how the tutorial deals with the dictionary check.
 ##    If this were my first Python rodeo I'd be tearing my hair out.
     
     item_added=input("What would you like?: ")
+    item_added=item_added.lower()
     if item_added in itemsDict:
         print("Thank-you for purchasing " +item_added)
+        item_qty=int(input("How many would you like?: "))
+        shoppingDict.update({item_added:{"quantity":item_qty, "subtotal":itemsDict[item_added]*item_qty}})
+        print(shoppingDict)
+        
     else:
         print("We don't have that... yet.")
         
